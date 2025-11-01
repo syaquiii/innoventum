e0e23a96ebfcba7cd680a09aac812722be499f32
syaquiii <msyauqioqi@gmail.com>
2025-11-02 01:10:57 +0700

    fixing anjing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "@prisma/engines"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("@prisma/client", "@prisma/engines");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
