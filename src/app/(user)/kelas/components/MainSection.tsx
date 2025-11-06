fbdda9183ac15340fd43a26fda0688892fa2137e
syaquiii <msyauqioqi@gmail.com>
2025-11-06 09:04:46 +0700

    feat: add logo on footer

---
+++ src/app/(user)/kelas/components/MainSection.tsx
@@ -0,0 +1,9 @@
+const MainSection = () => {
+  return (
+    <div className="min-h-160 bg-dark">
+      <div className="mycontainer py-20">a</div>
+    </div>
+  );
+};
+
+export default MainSection;
---
+++ src/app/(user)/kelas/container/KelasContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import MainSection from "../components/MainSection";
+
+const KelasContainer = () => {
+  return (
+    <main>
+      <Hero />
+      <MainSection />
+    </main>
+  );
+};
+
+export default KelasContainer;
